573eb05d6659bec5faed68b4fcfaba4a04274b44
amrita-20 <sona20dubey23@gmail.com>
2024-10-26 23:49:13 -0700

    scaffolding of shopping website with initial page creation and routing setup

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,23 +1,22 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+ <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/cart">Cart</RouterLink>
+        <RouterLink to="/products">Products</RouterLink>
       </nav>
     </div>
-  </header>
-
-  <RouterView />
+  </header> -->
+  <h1>Home</h1>
+  <router-view />
 </template>
 
 <style scoped>
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,21 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ShoppingCartPage from '@/pages/ShoppingCartPage.vue'
+import ProductsPage from '@/pages/ProductsPage.vue'
+import ProductDetailsPage from '@/pages/ProductDetailsPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/cart',
+      name: 'cart',
+      component: ShoppingCartPage
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/products',
+      name: 'products',
+      component: ProductsPage
+    },
+    {
+      path: '/products/:id',
+      name: 'productDetails',
+      component: ProductDetailsPage
     }
   ]
 })
