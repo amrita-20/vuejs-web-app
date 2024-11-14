ee18f7297c9f38e0c13ead60dfe57d2dbfae5c2a
amrita-20 <sona20dubey23@gmail.com>
2024-11-13 22:41:46 -0800

    simplified the cartItems by removing repetetive data and populated data inside cart from product

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -8,6 +8,13 @@ const app = express();
 
 app.use(express.json());
 
+function populatedCartIds (ids) {
+    return ids.map(cartId => {
+        const product = products.find(product => product.id === cartId);
+        return product;
+    })
+}
+
 app.get("/hello", (req, res) => {
     res.send("Hello World");
 
@@ -18,7 +25,8 @@ app.get("/products", (req, res) => {
 })
 
 app.get("/cart", (req, res) => {
-    res.json(cartItems);
+    const populatedCart = populatedCartIds(cartItems);
+    res.json(populatedCart);
 })
 
 app.get("/products/:id", (req, res) => {
@@ -29,16 +37,18 @@ app.get("/products/:id", (req, res) => {
 app.post("/cart", (req, res) => {
     const productId = req.body.id;
     const product = products.find((product) => product.id === productId);
-    product && cartItems.push(product);
-    res.json(cartItems);
+    product && cartItems.push(productId);
+    const populatedCartItems = populatedCartIds(cartItems);
+    res.json(populatedCartItems);
 })
 
 app.delete("/cart/:id", (req, res) => {
-    cartItems = cartItems.filter((item) => item.id !== req.params.id);
-    res.json(cartItems);
+    cartItems = cartItems.filter((item) => item !== req.params.id);
+    const populatedCartItems = populatedCartIds(cartItems);
+    res.json(populatedCartItems);
 })
 
 
 app.listen(8000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 8000");
 });
