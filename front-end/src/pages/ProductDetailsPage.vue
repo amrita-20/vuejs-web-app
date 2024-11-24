542125ac648e3bc43b1584397f07e0c037df8742
amrita-20 <sona20dubey23@gmail.com>
2024-11-24 00:18:40 -0800

    added dynamic control of add to cart

--- front-end/src/pages/ProductDetailsPage.vue
+++ front-end/src/pages/ProductDetailsPage.vue
@@ -6,7 +6,8 @@
         <div class="product-details">
             <h1>{{ product.name }}</h1>
             <h3 class="price">{{ product.price }}</h3>
-            <button @click="addToCart" class="add-to-cart">Add to Cart</button>
+            <button @click="addToCart" class="add-to-cart" v-if="!isInCart">Add to Cart</button>
+            <button @click="addToCart" class="grey-button" v-if="isInCart">Item is already in cart</button>
         </div>
     </div>
     <div v-if="!product">
@@ -15,14 +16,19 @@
 </template>
 
 <script>
-import { products } from '@/temp-data';
 import NotFoundPage from './NotFoundPage.vue';
 import axios from 'axios';
 export default {
     name: 'ProductDetailsPage',
     data() {
         return {
-            product: null
+            product: null,
+            cartItems: []
+        }
+    },
+    computed: {
+        isInCart() {
+            return this.cartItems.some(item => item.id === this.$route.params.id);
         }
     },
     methods: {
@@ -41,6 +47,9 @@ export default {
     async created(){
         const response = await axios.get(`/api/products/${this.$route.params.id}`);
         this.product = response.data;
+
+        const cartResponse = await axios.get('/api/users/12345/cart');
+        this.cartItems = cartResponse.data;
     }
 }
 </script>
